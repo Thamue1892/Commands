078926fa4107b5b854d71b8616d255990da0b103
ThaymesCoder <thamue1892@gmail.com>
2022-02-15 04:06:25 +0200

    Added Migrations
    Modified the Command class using data annotations.Added connection string
    and ran our first migration.

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commands.Model
 {
   public class Command
   {
+    [Key]
     public int Id { get; set; }
     
+    [Required]
+    [MaxLength(250)]
     public string HowTo { get; set; }
     
+    [Required]
     public string Line { get; set; }
     
+    [Required]
     public string Platform { get; set; }
     
   }
