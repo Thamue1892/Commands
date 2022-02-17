f07657c7d1ca8e1a65e08a359cab9509d18cb95d
ThaymesCoder <thamue1892@gmail.com>
2022-02-17 12:17:11 +0200

    Added Data Annotations
    Add Annotations to CommandCreateDto to avoid 500 Errors.

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commands.Dtos
 {
   public class CommandCreateDto
   {
+    [Required]
+    [MaxLength(250)]
     public string HowTo { get; set; }
 
-    public string Line { get; set; }
+    [Required]
+     public string Line { get; set; }
+
+    [Required]
     public string Platform { get; set; }
     
   }
